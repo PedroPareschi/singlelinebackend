1b095678347dfc098fab990b16de0fc0fd22770c
pedropareschi <pedropareschi.dev@gmail.com>
2025-04-18 22:07:02 +0100

    First commit

---
+++ src/main/java/com/cepel/unifilarbackend/UnifilarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cepel.unifilarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifilarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifilarBackendApplication.class, args);
+	}
+
+}
