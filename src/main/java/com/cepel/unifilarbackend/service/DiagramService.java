834872b1e4ab11d5ec163a85c238290d0bd4a2ee
pedropareschi <pedropareschi.dev@gmail.com>
2025-04-19 17:41:30 +0100

    Filter by type

--- src/main/java/com/cepel/unifilarbackend/service/DiagramService.java
+++ src/main/java/com/cepel/unifilarbackend/service/DiagramService.java
@@ -4,6 +4,7 @@ import com.cepel.unifilarbackend.entity.Component;
 import com.cepel.unifilarbackend.entity.Diagram;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -12,10 +13,14 @@ public class DiagramService {
 
     public Diagram filterHiddenItems(Diagram request) {
         Diagram response = new Diagram();
-        response.setComponents(request.getComponents().stream().filter(component -> component.getHidden() != null && !component.getHidden()).toList());
+        response.setComponents(request.getComponents().stream().filter(
+                component -> component.getHidden() != null && !component.getHidden()
+                        && (Objects.equals(component.getType(), "bus")
+                        || Objects.equals(component.getType(), "machine")
+                        || Objects.equals(component.getType(), "load"))).toList());
         Set<String> ids = response.getComponents().stream().map(Component::getId).collect(Collectors.toSet());
         response.setConnections(request.getConnections().stream().filter(
-                connection -> ids.contains(connection.getSource()) && ids.contains(connection.getTarget()))
+                        connection -> ids.contains(connection.getSource()) && ids.contains(connection.getTarget()))
                 .toList());
         return response;
     }
